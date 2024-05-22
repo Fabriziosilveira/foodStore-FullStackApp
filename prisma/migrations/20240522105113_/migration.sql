4537c1dabee4f11df9627305636cd468db21e727
Fabrizio Silveira <silveira6602@gmail.com>
2024-05-22 07:54:22 -0300

    :bug: fix: Remake a new model schema

---
+++ prisma/migrations/20240522105113_/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `firsName` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `secoundName` on the `users` table. All the data in the column will be lost.
+  - Added the required column `fullName` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_users" ("createdAt", "email", "id", "password") SELECT "createdAt", "email", "id", "password" FROM "users";
+DROP TABLE "users";
+ALTER TABLE "new_users" RENAME TO "users";
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+PRAGMA foreign_key_check("users");
+PRAGMA foreign_keys=ON;
