5031731e454d24ce21d14f93bf1dab81566c3e90
Fabrizio Silveira <silveira6602@gmail.com>
2024-05-23 22:17:07 -0300

    :sparkles: feat: Implement CORS on server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
-import { UserValidate } from './interfaces/user.interface';
 import { FastifyInstance, fastify } from "fastify";
 import { userRoutes } from './routes/user.routes';
+import fastifyCors from '@fastify/cors';
 
 const app: FastifyInstance = fastify();
+
+app.register(fastifyCors, {
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization']
+});
 
 app.register(userRoutes, {
     prefix: '/users',
@@ -12,7 +18,10 @@ app.register(userRoutes, {
     prefix: '/login',
 })
 
-app.listen({port: 3100}, () => {
-    console.log("Servidor rodando na porta 3100")
-})
-
+app.listen({ port: 3100 }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Servidor rodando na porta ${address}`);
+});
