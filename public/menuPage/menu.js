f2d402722140a0e7743de03cdf7a4f567af54387
Fabrizio Silveira <silveira6602@gmail.com>
2024-05-30 15:31:06 -0300

    :boom: fix: Menu page

--- public/menuPage/menu.js
+++ public/menuPage/menu.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async() => {
     const cartCountElement = document.getElementById('cartCount');
     const addToCartButtons = document.querySelectorAll('.add-to-cart');
+    const cartCount = document.getElementById('cartCount');
+    const cartButton = document.getElementById('cartButton');
+    
+    try {
+        const response = await fetch('http://localhost:3100/menu/products', {
+            method: 'POST'
+        });
+        if (!response.ok) {
+            throw new Error('Falha ao criar produtos');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
 
     addToCartButtons.forEach(button => {
         button.addEventListener('click', () => {
@@ -8,4 +21,16 @@ document.addEventListener('DOMContentLoaded', () => {
             cartCountElement.textContent = currentCount + 1;
         });
     });
+
+    const loadCartCount = () => {
+        const count = localStorage.getItem('cartCount');
+        cartCount.innerText = count ? count : '0';
+    };
+    
+    loadCartCount();
+
+    cartButton.addEventListener('click', () => {
+        window.location.href = '../checkoutPage/checkout.html';
+    });
+
 });
